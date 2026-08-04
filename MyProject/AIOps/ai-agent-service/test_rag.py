1fc505a2cfea985febb245772953661578d6554d
availgopi <125034049+availgopi@users.noreply.github.com>
2026-08-04 13:52:40 +0530

    Add files via upload

--- MyProject/AIOps/ai-agent-service/test_rag.py
+++ MyProject/AIOps/ai-agent-service/test_rag.py
@@ -1,1 +1,8 @@
+from app.rag.vector_store import create_vector_store
 
+db = create_vector_store()
+
+results = db.similarity_search("SQL connection timeout")
+
+for doc in results:
+    print(doc.page_content)

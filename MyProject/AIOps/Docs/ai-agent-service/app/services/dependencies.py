7f3a7909b8e3ff89cd42ade2a751bce04952a388
availgopi <125034049+availgopi@users.noreply.github.com>
2026-08-04 13:38:14 +0530

    Delete MyProject/AIOps/Docs/ai-agent-service directory

--- MyProject/AIOps/Docs/ai-agent-service/app/services/dependencies.py
+++
@@ -1,6 +0,0 @@
-from app.repository.incident_repository import IncidentRepository
-from app.services.incident_service import IncidentService
-
-repository = IncidentRepository()
-
-incident_service = IncidentService(repository)
